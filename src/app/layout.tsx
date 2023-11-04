8b2977d7407f55de693e2d8bf3cfe5b95c1cef05
Janyk Steenbeek <me@janyk.dev>
2023-11-04 21:51:44 +0100

    Refactor layout.tsx for improved readability and organization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,36 @@
 import "~/styles/globals.css";
 
-import { Inter } from "next/font/google";
-import { headers } from "next/headers";
+import {Inter} from "next/font/google";
+import {headers} from "next/headers";
 
-import { TRPCReactProvider } from "~/trpc/react";
+import {TRPCReactProvider} from "~/trpc/react";
 import Header from "~/app/_components/header";
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
+    subsets: ["latin"],
+    variable: "--font-sans",
 });
 
+export const runtime = 'edge';
+
 export const metadata = {
-  title: "Find all YouTube thumbnails for a channel or video | thumbnails.yt",
-  description: "Easily find all full-size live & A/B test YouTube thumbnails for channel or video",
+    title: "Find all YouTube thumbnails for a channel or video | thumbnails.yt",
+    description: "Easily find all full-size live & A/B test YouTube thumbnails for channel or video",
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`thumbnails-yt font-sans ${inter.variable} bg-cover bg-[url(/bg-thumbs.png)]`}>
+    return (
+        <html lang="en">
+        <body className={`thumbnails-yt font-sans ${inter.variable} bg-cover bg-[url(/bg-thumbs.png)]`}>
         <TRPCReactProvider headers={headers()}>
-          <Header/>
-          {children}
+            <Header/>
+            {children}
         </TRPCReactProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
