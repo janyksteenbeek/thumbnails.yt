a174fc499373b2687cb2258cf5740ead0e18c09c
Janyk Steenbeek <me@janyk.dev>
2023-11-16 21:34:05 +0100

    style: Update footer padding

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="py-8 lg:py-16">
+        <footer className="px-2 py-8 lg:py-16">
             <p className="text-center text-xs leading-5 text-gray-400">
                 A project by <Link href="https://x.com/janyksteenbeek" target="_blank">Janyk Steenbeek</Link>, not
                 affiliated with Google &middot; <Link href="https://github.com/sponsors/janyksteenbeek"
